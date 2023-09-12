7a43f2f33b96df31bb1271d0ff3405133d9a3728
SofyanWB <sofyanwibowo@outlook.com>
2023-09-12 09:27:33 +0700

    Sofyan Update 2

--- application/views/_partials/navbar.php
+++ application/views/_partials/navbar.php
@@ -1,43 +1,39 @@
 		<nav id="navbar-main" class="navbar navbar-main navbar-expand-lg navbar-light">
-		    <div class="container position-relative">
-		        <a class="navbar-brand mr-lg-5" href="<?php echo base_url(); ?>">
-		            <img class="navbar-brand-dark" src="<?php echo base_url(); ?>assets/img/brand/light.svg" alt="Logo light">
-		            <img class="navbar-brand-light" src="<?php echo base_url(); ?>assets/img/brand/dark.svg" alt="Logo dark">
-		        </a>
-		        <div class="navbar-collapse collapse" id="navbar_global">
-		            <div class="navbar-collapse-header">
-		                <div class="row">
-		                    <div class="col-6 collapse-brand">
-		                        <a href="<?php echo base_url(); ?>">
-		                            <img src="<?php echo base_url(); ?>assets/img/brand/secondary.svg" alt="Logo mobile">
-		                        </a>
-		                    </div>
-		                </div>
-		            </div>
-		            <ul class="navbar-nav">
-		                <li>
-		                    <a class="dropdown-item" href="<?php echo base_url(); ?>faq" style="border-radius: 30px;">FAQ</a>
-		                </li>
-		            </ul>
-		        </div>
-		        <div class="d-flex align-items-center">
-		            <ul class="flex-row list-style-none d-sm-flex">
-		                <li class="nav-item mr-3">
-		                    <a href="https://www.twitter.com/jakartasatudki/" target="_blank" class="btn btn-link" data-toggle="tooltip" data-placement="bottom" title="Twitter">
-		                        <i class="fab fa-twitter"></i>
-		                    </a>
-		                </li>
-		                <li class="nav-item mr-3">
-		                    <a href="https://www.instagram.com/jakartasatudki/" target="_blank" class="btn btn-link" data-toggle="tooltip" data-placement="bottom" title="Instagram">
-		                        <i class="fab fa-instagram"></i>
-		                    </a>
-		                </li>
-		                <li class="nav-item mr-3">
-		                    <a href="https://www.youtube.com/@JakartaSatuDKI/" target="_blank" class="btn btn-link" data-toggle="tooltip" data-placement="bottom" title="Youtube">
-		                        <i class="fab fa-youtube"></i>
-		                    </a>
-		                </li>
-		            </ul>
-		        </div>
-		    </div>
+			<div class="container position-relative">
+				<a class="navbar-brand mr-lg-5" href="<?php echo base_url(); ?>">
+					<img class="navbar-brand-dark" src="<?php echo base_url(); ?>assets/img/brand/light.svg" alt="Logo light">
+					<img class="navbar-brand-light" src="<?php echo base_url(); ?>assets/img/brand/dark.svg" alt="Logo dark">
+				</a>
+				<div class="flex-row list-style-none d-flex align-items-center" id="navbar_global">
+					<div class="navbar">
+						<a href="<?php echo base_url(); ?>">
+							<img src="<?php echo base_url(); ?>assets/img/brand/secondary.svg" alt="Logo mobile">
+						</a>
+					</div>
+					<ul class="navbar-nav">
+						<li>
+							<a class="dropdown-item" href="<?php echo base_url(); ?>faq" style="border-radius: 30px;">FAQ</a>
+						</li>
+					</ul>
+				</div>
+				<div class="d-flex align-items-center">
+					<ul class="d-flex list-style-none">
+						<li class="nav-item mr-3">
+							<a href="https://www.twitter.com/jakartasatudki/" target="_blank" class="btn btn-link" data-toggle="tooltip" data-placement="bottom" title="Twitter">
+								<i class="fab fa-twitter"></i>
+							</a>
+						</li>
+						<li class="nav-item mr-3">
+							<a href="https://www.instagram.com/jakartasatudki/" target="_blank" class="btn btn-link" data-toggle="tooltip" data-placement="bottom" title="Instagram">
+								<i class="fab fa-instagram"></i>
+							</a>
+						</li>
+						<li class="nav-item mr-3">
+							<a href="https://www.youtube.com/@JakartaSatuDKI/" target="_blank" class="btn btn-link" data-toggle="tooltip" data-placement="bottom" title="Youtube">
+								<i class="fab fa-youtube"></i>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
 		</nav>
